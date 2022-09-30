64def69b937c6ed7676b363e2240d40f0baa05fe
AdrianRWilson <114436469+AdrianRWilson@users.noreply.github.com>
2022-09-30 12:50:36 -0700

    orbiter

---
+++ 928 orbiter lab/orbiter.js
@@ -0,0 +1,33 @@
+var Orbiter = function (x, y, r, i, orbiterAmount) {
+    this.planetloc = new JSVector(x, y);
+    this.loc = this.planetloc;
+    this.r = r;
+    this.orbitalDistance = 30;
+    this.angleVelocity = 0.1;
+    this.orbiterAmount = orbiterAmount;
+    this.increment = i;
+    console.log(this.increment);
+    this.angle = this.increment * (360/this.orbiterAmount);
+    console.log(this.angle);
+}
+
+Orbiter.prototype.update = function (x, y) {
+    this.planetloc.x = x;
+    this.planetloc.y = y;
+    this.angle += this.angleVelocity;
+    this.loc.x = this.planetloc.x + (this.orbitalDistance * Math.cos(this.angle));
+    this.loc.y = this.planetloc.y + (this.orbitalDistance * Math.sin(this.angle));
+};
+
+Orbiter.prototype.render = function() {
+    context.beginPath(); 
+    context.arc(this.loc.x, this.loc.y, this.r, 0, 2 * Math.PI);
+    context.strokeStyle = "black";
+    context.fillStyle = "red";
+    context.fill();
+}
+
+Orbiter.prototype.run = function(x, y) {
+    this.update(x, y);
+    this.render();
+}
